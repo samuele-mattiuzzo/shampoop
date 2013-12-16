3fd73cb5ef496b6af8840271d8ad21cf1815f54f
Kevin Morgan <kevin.d.morgan@gmail.com>
2013-12-16 23:50:04 +0000

    Centering vertically (in an amazingly awkward way).

--- shampoop/www/js/shampoop.js
+++ shampoop/www/js/shampoop.js
@@ -28,6 +28,7 @@ var shampoop = {
             $poops = $poopList.find('li');
 
         this.setVisibilities($poops);
+        this.setPositions($poops);
     },
 
     setVisibilities: function ($poops) {
@@ -36,5 +37,45 @@ var shampoop = {
 
         $first.addClass('visible');
         $rest.addClass('hidden');
+    },
+
+    setPositions: function ($poops) {
+        _.each($poops, function (poop) {
+            var $poop = $(poop),
+                viewPortDimensions = this.getViewPortDimensions();
+            this.setPosition($poop, viewPortDimensions);
+        }, this);
+    },
+
+    getViewPortDimensions: function () {
+        var $document = $(document);
+
+        return {
+            height: $document.height(),
+            width: $document.width()
+        };
+    },
+
+    setPosition: function ($el, viewPortDimensions) {
+        var elementDimensions = this.getElementDimensions($el);
+
+        $el.css('position', 'relative');
+
+        this.setMarginTop($el, elementDimensions.height, viewPortDimensions.height);
+    },
+
+    getElementDimensions: function ($el) {
+        var $img = $el.find('img');
+
+        return {
+            height: $img.height(),
+            width: $img.width()
+        };
+    },
+
+    setMarginTop: function ($el, elementHeight, viewPortHeight) {
+        var marginTop = (viewPortHeight - elementHeight) / 2;
+
+        $el.css('margin-top', marginTop);
     }
 };
